93ad800f686b5ccb062c82109a707775600e8af4
rafallyczek <rafal.lyczek@gmail.com>
2020-10-08 23:41:50 +0200

    Changed code of ThemeController's theme() method to redirect to page from which theme change request came.

--- src/main/java/paw/project/calendarapp/controller/ThemeController.java
+++ src/main/java/paw/project/calendarapp/controller/ThemeController.java
@@ -3,12 +3,16 @@ package paw.project.calendarapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ThemeController {
 
     @PostMapping("/theme")
-    public String home() {
-        return "redirect:/";
+    public String theme(HttpServletRequest request) {
+        int index = request.getHeader("Referer").indexOf("/",8);
+        String redirectURL = request.getHeader("Referer").substring(index);
+        return "redirect:"+redirectURL;
     }
 
 }
