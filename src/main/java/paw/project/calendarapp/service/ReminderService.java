a9b24068685b4bba5a770b270abe27312f2a761a
rafallyczek <rafal.lyczek@gmail.com>
2021-01-17 20:33:43 +0100

    Set up server-side websocket configuration for specific user and client-side websocket connection with STOMP and SockJS (Work in progress); Renamed ReminderService's emailReminder method to reminder as it no longer sends email-only reminders (Now sends messages with SimpMessagingTemplate using websocket).

--- src/main/java/paw/project/calendarapp/service/ReminderService.java
+++ src/main/java/paw/project/calendarapp/service/ReminderService.java
@@ -1,6 +1,7 @@
 package paw.project.calendarapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import paw.project.calendarapp.model.Note;
@@ -20,17 +21,19 @@ public class ReminderService {
     private EmailService emailService;
     private UserService userService;
     private NoteRepository noteRepository;
+    private SimpMessagingTemplate template;
 
     @Autowired
-    public ReminderService(EmailService emailService, UserService userService, NoteRepository noteRepository){
+    public ReminderService(EmailService emailService, UserService userService, NoteRepository noteRepository, SimpMessagingTemplate template){
         this.emailService = emailService;
         this.userService = userService;
         this.noteRepository = noteRepository;
+        this.template = template;
     }
 
     //Wyślij wiadomość email z przypomnieniem; 30*60*1000 = 1800000 (wywołaj co 30 minut); 60*1000 = 60000 (pierwsze wywołanie po 1 minucie)
     @Scheduled(fixedRate = 1800000, initialDelay = 60000)
-    public void emailReminder() throws MessagingException {
+    public void reminder() throws MessagingException {
         List<Note> notes = new ArrayList<>();
         noteRepository.findAll().forEach(notes::add);
         for(Note note : notes){
@@ -46,6 +49,7 @@ public class ReminderService {
                     emailService.sendEmail(user.getEmail(),"Wydarzenie","<p>Przypomnienie o nadchodzącym wydarzeniu.</p><p>Wydarzenie: <b>" +
                             note.getTitle()+"</b></p>"+"<p>Opis: <b>"+note.getContent()+"</b></p><p>Zaplanowano na: <b>"+localDateTime.toLocalDate().toString() +
                             "</b>, godzina: <b>"+localDateTime.toLocalTime().toString()+"</b></p>");
+                    template.convertAndSendToUser(user.getUsername(),"/queue/message","Wiadomość");
                 }
             }
         }
