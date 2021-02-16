09e9194f3169c687f567a9ee2c8028a177a093bf
rafallyczek <rafal.lyczek@gmail.com>
2021-02-16 16:32:19 +0100

    Changed LocaleHolder's scope to session.

--- src/main/java/kanbancalendar/project/app/component/LocaleHolder.java
+++ src/main/java/kanbancalendar/project/app/component/LocaleHolder.java
@@ -2,10 +2,12 @@ package kanbancalendar.project.app.component;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.Locale;
 
 @Component
+@SessionScope
 @Data
 public class LocaleHolder {
 
